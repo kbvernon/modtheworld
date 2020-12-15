b8bb98344765e410908e5d0460407358c2d23655
Kenneth Blake Vernon <kenneth.b.vernon@gmail.com>
2020-12-15 15:12:48 -0700

    added before-script

---
+++ R/before_script.R
@@ -0,0 +1,16 @@
+
+library(dplyr)
+library(ggplot2)
+library(here)
+library(knitr)
+library(kableExtra)
+
+knitr::opts_chunk$set(echo = TRUE,
+                      warning = FALSE,
+                      message = FALSE,
+                      fig.align = "center")
+
+
+set.seed(2020)
+
+source(here("R/insert_intro.R"))
